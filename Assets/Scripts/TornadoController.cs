d8207c40bb8166545dc2a7313076002c715afafe
tzverg <tzverg@gmail.com>
2019-10-28 16:04:01 +0200

    first commit with scaled tornado

---
+++ Assets/Scripts/TornadoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class TornadoController : MonoBehaviour
+{
+    [Range(1F, 2F)][SerializeField] private float tornadoScale = 1F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckLocalScale();
+    }
+
+    private void CheckLocalScale()
+    {
+        if (transform.localScale.x != tornadoScale || transform.localScale.y != tornadoScale)
+        {
+            transform.localScale = new Vector3(tornadoScale, 1F, tornadoScale);
+        }
+    }
+}
